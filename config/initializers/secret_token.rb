dda113e9c580b005678e97de078af4f8a6b04931
Ricardo Gonzalez Tellez <ricardo@softco.mx>
2014-04-30 16:42:36 -0500

    Primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gazpacho::Application.config.secret_key_base = '1dbf6d0c536f046c6b9d3bc6417e2ae421e3efbc6aa025a33c9348be85e375ed448a356eabc907c120f769014910614c4f8ed95379cb5748611ff2018993dc56'
